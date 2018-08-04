3f8d825198cbde68c16bcb3f92d8877d7e53f157
zhengguangjing <zimuka@sina.com>
2018-08-04 17:45:39 +0800

    消息推送优化

--- src/main/java/com/zimuka/peers/service/impl/WxTemplateServiceImpl.java
+++ src/main/java/com/zimuka/peers/service/impl/WxTemplateServiceImpl.java
@@ -29,12 +29,12 @@ public class WxTemplateServiceImpl implements WxTemplateService {
         WechatTemplate wechatTemplate = new WechatTemplate();
         wechatTemplate.setTouser(openId);//用户ID
         wechatTemplate.setForm_id(formId);//表单ID
-        wechatTemplate.setTemplate_id("kW19kb8Tq0WdL-cFQq61pCxkKXpVmsopcgrflt4e_zA");//模板ID
-        wechatTemplate.setPage("");
+        wechatTemplate.setTemplate_id("kW19kb8Tq0WdL-cFQq61pJjToO7fRL7Vs1MIk06c-cA");//模板ID
+        wechatTemplate.setPage("/pages/findmore/findmore");
 
         Map<String, TemplateData> data = new HashMap<String, TemplateData>();
         data.put("keyword1", new TemplateData("您的电子名片已创建成功，点击查看", "#173177"));
-        data.put("keyword1", new TemplateData(makeTime, "#173177"));
+        data.put("keyword2", new TemplateData(makeTime, "#173177"));
         wechatTemplate.setData(data);
 
         AccessToken accessToken = WxTemplateUtil.getAccessToken(miniAppBean.getAppId(), miniAppBean.getAppSecret());
