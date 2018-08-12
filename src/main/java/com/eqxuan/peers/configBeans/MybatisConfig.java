fd00629aa40fe15d883f051e5b8c88f777d91788
mxl0529 <mxl_mature@163.com>
2018-08-12 15:27:33 +0800

    添加sql耗时日志

---
+++ src/main/java/com/eqxuan/peers/configBeans/MybatisConfig.java
@@ -0,0 +1,79 @@
+package com.eqxuan.peers.configBeans;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import com.eqxuan.peers.interceptor.SqlCostInterceptor;
+import com.github.pagehelper.PageHelper;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
+
+/**
+ * @Auther: Mature
+ * @Date: 2018/8/12 14:56
+ * @Description:
+ */
+@Configuration
+@EnableTransactionManagement
+public class MybatisConfig implements TransactionManagementConfigurer {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactoryBean(PageHelper pageHelper) throws IOException {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        bean.setMapperLocations(resolver.getResources("classpath:sqlmap/*.xml"));
+        bean.setTypeAliasesPackage("com.eqxuan.peers");
+
+        bean.setDataSource(dataSource);
+        bean.setPlugins(new Interceptor[]{new SqlCostInterceptor()});
+        try {
+            return bean.getObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    public ConfigurationCustomizer mybatisConfigurationCustomizer() {
+        return configuration -> configuration.setMapUnderscoreToCamelCase(true);
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    public PageHelper pageHelper() {
+        PageHelper pageHelper = new PageHelper();
+        Properties p = new Properties();
+        p.setProperty("offsetAsPageNum", "true");
+        p.setProperty("rowBoundsWithCount", "true");
+        p.setProperty("reasonable", "true");
+        p.setProperty("dialect", "mysql");
+        pageHelper.setProperties(p);
+        return pageHelper;
+    }
+}
