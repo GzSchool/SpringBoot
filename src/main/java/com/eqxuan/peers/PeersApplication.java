bd6c9d86aa8638fb140abc8b876cbc9d2bd52f87
zhengguangjing <zimuka@sina.com>
2018-08-26 19:12:40 +0800

    图片大小限制

--- src/main/java/com/eqxuan/peers/PeersApplication.java
+++ src/main/java/com/eqxuan/peers/PeersApplication.java
@@ -3,7 +3,11 @@ package com.eqxuan.peers;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.servlet.MultipartConfigElement;
 
 @EnableTransactionManagement
 @SpringBootApplication
@@ -12,5 +16,15 @@ public class PeersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PeersApplication.class, args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // 文件最大
+        factory.setMaxFileSize("5MB");
+        // 设置总上传数据总大小
+        factory.setMaxRequestSize("20MB");
+        return factory.createMultipartConfig();
     }
 }
